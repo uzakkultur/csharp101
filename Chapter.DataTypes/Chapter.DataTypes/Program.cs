7fb479a9f3c1ce8206584c564f27aa8d32d7fc6e
uzakkultur <uzakkultur@gmail.com>
2016-06-05 17:39:23 +0300

    csharp byte data types

---
+++ Chapter.DataTypes/Chapter.DataTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter.DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sayisal veri tipleri
+            // Tamsayi Degisken Tipleri (Integer Variable Types)
+            byte nuri = 13; //0 ile 255 arasi sayisal degeri belirtir.
+        }
+    }
+}
