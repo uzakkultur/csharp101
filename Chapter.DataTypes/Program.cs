dff2164937935c0fcd1290b60ed3eaf065a3ea85
uzakkultur <uzakkultur@gmail.com>
2016-06-05 18:02:33 +0300

    full of csharp byte data types

--- Chapter.DataTypes/Program.cs
+++ Chapter.DataTypes/Program.cs
@@ -12,7 +12,14 @@ namespace Chapter.DataTypes
         {
             // Sayisal veri tipleri
             // Tamsayi Degisken Tipleri (Integer Variable Types)
-            byte nuri = 13; //0 ile 255 arasi sayisal degeri belirtir.
+            byte nuri = 13; //0 ile 255 arasi sayisal degeri belirtir. (1Byte)
+            sbyte snuri = -13; //-128 ile 127 arasi sayisal degeri belirtir. (1Byte)
+            short shnuri = 7; //-32768 ile 32767 arasi sayisal degeri belirtir. (2Byte)
+            ushort ushnuri = 70; //0 ile 65535 arasi sayisal degeri belirtir. (2Byte)
+            int intnuri = 700; //-21474843648 ile 2147483648 arasi sayisal degeri belirtir. (4Byte)
+            uint uintnuri = 701; //0 ile 4294967295 arasi sayisal degeri belirtir. (4Byte)
+            long longnuri = 7000; //-10^20 ile 10^20 arasi sayisal degeri belirtir. (8Byte)
+            ulong ulongnuri = 70001; //0 ile 2*10^20 arasi sayisal degeri belirtir. (8Byte)
         }
     }
 }
